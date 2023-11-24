46fdaad3f4c331b6b73fc532bafd6413f800b025
Y30L <101905747+byeongyeolahn@users.noreply.github.com>
2023-11-24 11:39:17 +0900

    Create get_userassist.py
    Create get_userassist.py

---
+++ artifact(18-26)/get_userassist.py
@@ -0,0 +1,39 @@
+import winreg
+import codecs
+import csv
+
+def csv_writer(files):
+    with open('userassist.csv', 'w', newline="") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(['File Name'])
+        
+        for i in range(len(files)):
+            csv_writer.writerow([files[i]])
+    
+def GetRegValue_userassist(key_path):
+    userassist_list = []
+    # try:
+    with winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path) as key:
+        f = open('userassist.csv', 'w')
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(['File Name'])
+        for i in range(0, winreg.QueryInfoKey(key)[0]):
+            userassist = winreg.EnumKey(key, i)
+            userassist_key_path = f"{key_path}\{userassist}\count"
+            
+            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, userassist_key_path) as userassist_key:
+                for j in range(0, winreg.QueryInfoKey(userassist_key)[1]):
+                    userassist_key_name = winreg.EnumValue(userassist_key, j)
+                    try:
+                        decrypted_userassist = codecs.decode(userassist_key_name[0], 'rot_13')
+                        userassist_list.append(decrypted_userassist)
+                    except:
+                        userassist_list.append(userassist_key_name[0])
+                        continue
+    # except:
+    #     print(f"[-] UserAssist 값 파싱 중 오류 발생({e})")
+    return userassist_list
+
+def GetUserAssist():
+    userassist_list = GetRegValue_userassist(r"Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist")                    
+    csv_writer(userassist_list)
