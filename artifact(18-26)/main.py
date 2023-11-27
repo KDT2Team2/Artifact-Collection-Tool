547d8f8ed8975daa2f9f963151fc0884503b2fa0
Y30L <101905747+byeongyeolahn@users.noreply.github.com>
2023-11-28 02:06:48 +0900

    Create main.py
    Create main.py

---
+++ artifact(18-26)/main.py
@@ -0,0 +1,12 @@
+#import get_userassist
+#import get_service
+#import get_autorun
+import get_browser
+#import get_lnkfile
+# import get_powershelllog
+# import get_recentfile  - x
+# import get_recyclebin
+# import get_registry!
+
+if __name__ == "__main__":
+    get_browser.main()
